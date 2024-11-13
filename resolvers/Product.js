0e402cfc6c67a835a4eb9b00230f55043496e5e9
Bharath <bharathb.w75@gmail.com>
2024-11-13 12:20:54 -0600

    final changes for product reviews

--- resolvers/Product.js
+++ resolvers/Product.js
@@ -1,6 +1,6 @@
 exports.Product = {
-  category: ({ categoryId }, args, { db }) => {
-    return db.categories.find((category) => category.id === categoryId);
+  category: ({ categoryId }, args, { categories }) => {
+    return categories.find((category) => category.id === categoryId);
   },
   reviews: ({ id }, { filter }, { reviews }) => {
     // Filter reviews belonging to the product
