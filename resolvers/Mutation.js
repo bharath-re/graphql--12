d71a29718992545b8810e4211c2efc1805d28fee
Bharath <bharathb.w75@gmail.com>
2024-11-13 16:24:16 -0600

    the add-review-changes

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -26,4 +26,19 @@ exports.Mutation = {
     products.push(newProduct);
     return newProduct;
   },
+
+  addReview: (parent, { input }, { reviews }) => {
+    const { date, title, comment, rating, productId } = input;
+
+    const newReview = {
+      id: uuid(),
+      date,
+      title,
+      comment,
+      rating,
+      productId,
+    };
+    reviews.push(newReview);
+    return newReview;
+  },
 };
